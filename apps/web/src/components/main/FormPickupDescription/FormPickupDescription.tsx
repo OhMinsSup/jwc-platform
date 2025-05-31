22339e54d453845a160d92cf42a23764d839b100
OhMinsSup <mins5190@gmail.com>
2025-06-01 01:49:35 +0900

    chore: 문구 수정

--- apps/web/src/components/main/FormPickupDescription/FormPickupDescription.tsx
+++ apps/web/src/components/main/FormPickupDescription/FormPickupDescription.tsx
@@ -21,9 +21,8 @@ export default function FormPickupDescription({ idx }: LazyComponentProps) {
 						부분참일 경우 참석 가능한 시간 혹은 픽업을 원하는 시간을 구체적으로
 						적어주세요. <br />
 						<strong>
-							경기광주역으로 도착하는 시간을 적어주셔야 합니다.
 							<br />
-							예시) 28일 12시에 경기광주역 도착
+							예시) 28일 12시에 교회 도착
 						</strong>
 					</>
 				}
