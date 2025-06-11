6061a1cb41f50884f0068c80e113a46083b5e7f1
OhMinsSup <mins5190@gmail.com>
2025-06-11 23:41:14 +0900

    chore: sentry 설정 수정

--- packages/observability/src/next-config.ts
+++ packages/observability/src/next-config.ts
@@ -5,9 +5,6 @@ export const sentryConfig: Parameters<typeof withSentryConfig>[1] = {
 	org: env.SENTRY_ORG,
 	project: env.SENTRY_PROJECT,
 
-	// Only print logs for uploading source maps in CI
-	silent: !env.CI,
-
 	/*
 	 * For all available options, see:
 	 * https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
