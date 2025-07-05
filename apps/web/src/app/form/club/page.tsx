b43fa8bbaeed43005ae983b82882236f81c003f7
OhMinsSup <mins5190@gmail.com>
2025-07-06 00:36:49 +0900

    add: server component 기능 제거

--- apps/web/src/app/form/club/page.tsx
+++ apps/web/src/app/form/club/page.tsx
@@ -1,12 +1,5 @@
 import { ClubListPage } from "~/components/club/ClubListPage";
-import { HydrateClient, orpc, prefetch } from "~/libs/orpc/server";
 
-export default async function Page() {
-	await prefetch(orpc.clubs.getAll.queryOptions());
-
-	return (
-		<HydrateClient>
-			<ClubListPage />
-		</HydrateClient>
-	);
+export default function Page() {
+	return <ClubListPage />;
 }
